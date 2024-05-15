0c3fde4542a37bdf208b21f1aa78453582a5570c
Atar Mor <atarmor92@gmail.com>
2024-05-15 09:44:59 +0300

    Fix mongo connection str on prod

--- config/dev.js
+++ config/dev.js
@@ -1,5 +1,5 @@
 export default  {
-  dbURL: 'mongodb+srv://atarmor92:FDby$wmDCTCu6wZ@cluster0.e3x2fqd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
-  // dbURL: 'mongodb://127.0.0.1:27017',
+  // dbURL: 'mongodb+srv://atarmor92:FDby$wmDCTCu6wZ@cluster0.e3x2fqd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+  dbURL: 'mongodb://127.0.0.1:27017',
   dbName : 'expenseDB'
 }
